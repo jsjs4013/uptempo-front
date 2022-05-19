48a0ca0d71f91350863466f4c3168d36d2a6ceb9
문주원 <iismoon@naver.com>
2022-05-19 17:27:19 +0900

    Add device page

--- components/SubNavbar.js
+++ components/SubNavbar.js
@@ -7,36 +7,25 @@ import ControlIcon from "../public/icons8-touchscreen-30.png"
 import DevicelIcon from "../public/icons8-smartphones-30.png"
 
 export default function Navbar() {
-    const [menuId, setMenuId] = useState(0);
     const navbarContent = [
-        {id:1, content:'Control', imageIcon:ControlIcon},
-        {id:2, content:'Device', imageIcon:DevicelIcon},
-        {id:3, content:'Setting', imageIcon:DevicelIcon}
+        {id:1, content:'Control', imageIcon:ControlIcon, route:'/'},
+        {id:2, content:'Device', imageIcon:DevicelIcon, route:'/device'},
+        {id:3, content:'Setting', imageIcon:DevicelIcon, route:'/'}
     ]
-    const menuImage = 'rotate-6'
-    const menuText = 'text-[#2b3d51]'
     const div = []
 
     for(let i=0;i<navbarContent.length;i++) {
         let menuContent = navbarContent[i]
-        console.log('Do something after counter has changed2', menuId);
 
         div.push(
-            <div id={menuContent.id}
-                onMouseEnter={(event) => {setMenuId(menuContent.id)}}
-                onMouseLeave={(event) => {setMenuId(0)}}>
-                <Link href='/'>
-                    <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 ml-4 rounded text-white font-bold items-center justify-center'>
+            <div>
+                <Link href={menuContent.route}>
+                    <a className='group lg:inline-flex lg:w-auto w-full px-3 py-2 ml-4 rounded text-white font-bold items-center justify-center'>
                         <Image
                             src={menuContent.imageIcon}
-                            className= {menuContent.id === menuId ? menuImage : null}
+                            className= 'group-hover:rotate-6'
                         />
-                        <span className={menuContent.id === menuId ? menuText : null}>
-                            {/* {useEffect(() => {
-                                console.log('Do something after counter has changed', menuId);
-                                
-                                }, [menuId])} */}
-                            {/* {console.log('Do something after counter has changed', menuId)} */}
+                        <span className='group-hover:text-[#2b3d51]'>
                             {menuContent.content}
                         </span>
                     </a>
