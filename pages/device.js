eab54a493f89aa73472cb6c6ebaf5382cc395d01
Joowon Moon <jsjs401@daum.net>
2022-05-30 18:05:15 +0900

    modal 적용했고 그 안에 라디오 버튼 적용중 headless ui로 적용중

--- pages/device.js
+++ pages/device.js
@@ -5,6 +5,9 @@ import Navbar from '../components/Navbar'
 import Layout from '../components/Layout'
 import dynamic from 'next/dynamic'
 
+import { Dialog, Transition, RadioGroup } from '@headlessui/react'
+import { Fragment, useState } from 'react'
+
 
 const DynamicDesktop = dynamic( // For no SSR
   () => import('../components/Device_screen').then((mod) => mod.Desktop),
@@ -18,6 +21,16 @@ const DynamicPhone = dynamic( // For no SSR
 
 export default function Device() {
     let currentPage = 2
+    const [isOpen, setIsOpen] = useState(false)
+    const [plan, setPlan] = useState('startup')
+
+    function closeModal() {
+        setIsOpen(false)
+      }
+    
+    function openModal() {
+    setIsOpen(true)
+    }
     
     return (
         <Layout>
@@ -51,63 +64,118 @@ export default function Device() {
                         </DynamicDesktop>
 
                         <DynamicPhone>
-                            <div className="space-y-3 tracking-widest mt-2 lg:w-1/5 lg:px-2 lg:space-y-2">
-                                <button className='flex w-36 items-center justify-center rounded-full bg-gradient-to-r from-cyan-500 to-gray-600'>
-                                    <p className='text-white font-bold mt-2 mb-2'>제조사</p>
-                                </button>
-                                <div className='flex flex-col items-left ml-7'>
-                                    <a href="#" className="block font-bold text-blue-900 hover:underline">ALL</a>
-                                    <a href="#" className="block font-medium text-gray-500 hover:underline">APPLE</a>
-                                    <a href="#" className="block font-medium text-gray-500 hover:underline">SAMSUNG</a>
-                                    <a href="#" className="block font-medium text-gray-500 hover:underline">LG</a>
-                                    <a href="#" className="block font-medium text-gray-500 hover:underline">etc</a>
+                        <div className="flex justify-around tracking-widest">
+                                <div className='inline-flex w-36 items-center justify-center rounded-full bg-gradient-to-r from-cyan-500 to-gray-600'>
+                                    <button
+                                        type="button"
+                                        onClick={openModal}
+                                        className="text-white font-bold mt-2 mb-2 w-full h-full"
+                                    >
+                                        제조사
+                                    </button>
                                 </div>
-                                <div className='flex w-36 items-center justify-center rounded-full bg-gradient-to-r from-cyan-500 to-gray-600'>
-                                    <p className='text-white font-medium mt-2 mb-2'>OS</p>
-                                </div>
-                                <div className='flex flex-col items-left ml-7'>
-                                    <a href="#" className="block font-bold text-blue-900 hover:underline">ALL</a>
-                                    <a href="#" className="block font-medium text-gray-500 hover:underline">APPLE</a>
-                                    <a href="#" className="block font-medium text-gray-500 hover:underline">ANDROID</a>
-                                    <a href="#" className="block font-medium text-gray-500 hover:underline">IOS</a>
-                                    <a href="#" className="block font-bold text-gray-500 hover:underline">etc</a>
+                                
+                                <div className='inline-flex w-36 items-center justify-center rounded-full bg-gradient-to-r from-cyan-500 to-gray-600'>
+                                    <button
+                                        type="button"
+                                        onClick={openModal}
+                                        className="text-white font-bold mt-2 mb-2 w-full h-full"
+                                    >
+                                        OS
+                                    </button>
                                 </div>
                             </div>
 
-                            <button className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" data-modal-toggle="defaultModal">
-                                Toggle modal
-                            </button>
+                            <Transition appear show={isOpen} as={Fragment}>
+                                <Dialog as="div" className="relative z-10" onClose={closeModal}>
+                                    <Transition.Child
+                                        as={Fragment}
+                                        enter="ease-out duration-100"
+                                        enterFrom="opacity-30"
+                                        enterTo="opacity-100"
+                                        leave="ease-in duration-100"
+                                        leaveFrom="opacity-100"
+                                        leaveTo="opacity-0"
+                                    >
+                                        <div className="fixed inset-0 bg-black bg-opacity-25" />
+                                    </Transition.Child>
 
-                            <div id="defaultModal" tabindex="-1" aria-hidden="true" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
-                                <div className="relative p-4 w-full max-w-2xl h-full md:h-auto">
-                                    
-                                    <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                                        
-                                        <div className="flex justify-between items-start p-4 rounded-t border-b dark:border-gray-600">
-                                            <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                                                Terms of Service
-                                            </h3>
-                                            <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModal">
-                                                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>  
-                                            </button>
-                                        </div>
-                                        
-                                        <div className="p-6 space-y-6">
-                                            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                                                With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
-                                            </p>
-                                            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                                                The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
-                                            </p>
-                                        </div>
-                                        
-                                        <div className="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
-                                            <button data-modal-toggle="defaultModal" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
-                                            <button data-modal-toggle="defaultModal" type="button" className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
+                                    <div className="fixed inset-0 overflow-y-auto">
+                                        <div className="flex min-h-full items-center justify-center p-4 text-center">
+                                            <Transition.Child
+                                                as={Fragment}
+                                                enter="ease-out duration-100"
+                                                enterFrom="opacity-0 scale-95"
+                                                enterTo="opacity-100 scale-100"
+                                                leave="ease-in duration-100"
+                                                leaveFrom="opacity-100 scale-100"
+                                                leaveTo="opacity-0 scale-95"
+                                            >
+                                                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                                                    <Dialog.Title
+                                                        as="h3"
+                                                        className="text-lg font-medium leading-6 text-gray-900"
+                                                    >
+                                                        제조사
+                                                    </Dialog.Title>
+                                                    <RadioGroup value={plan} onChange={setPlan}>
+                                                        <RadioGroup.Label>Plan</RadioGroup.Label>
+                                                        <RadioGroup.Option value="startup">
+                                                            {({ active, checked }) => (
+                                                                <>
+                                                                    <span
+                                                                    className={`
+                                                                        ${checked ? 'bg-indigo-600 ' : ''}
+                                                                        ${active ? 'ring-2 ring-indigo-500' : ''}
+                                                                    `}
+                                                                    />
+                                                                    Startup
+                                                                </>
+                                                            )}
+                                                        </RadioGroup.Option>
+                                                        <RadioGroup.Option value="business">
+                                                            {({ active, checked }) => (
+                                                                <>
+                                                                    <span
+                                                                    className={`
+                                                                        ${checked ? 'bg-indigo-600 ' : ''}
+                                                                        ${active ? 'ring-2 ring-indigo-500' : ''}
+                                                                    h-4 w-4 rounded-full`}
+                                                                    />
+                                                                    Startup2
+                                                                </>
+                                                            )}
+                                                        </RadioGroup.Option>
+                                                        <RadioGroup.Option value="enterprise">
+                                                            {({ active, checked }) => (
+                                                                <>
+                                                                    <span
+                                                                    className={`
+                                                                        ${checked ? 'bg-indigo-600 ' : ''}
+                                                                        ${active ? 'ring-2 ring-indigo-500' : ''}
+                                                                    h-4 w-4 rounded-full`}
+                                                                    />
+                                                                    Startup3
+                                                                </>
+                                                            )}
+                                                        </RadioGroup.Option>
+                                                    </RadioGroup>
+
+                                                    <div className="mt-4">
+                                                        <button
+                                                            type="button"
+                                                            className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                                                            onClick={closeModal}
+                                                            >
+                                                            선택
+                                                        </button>
+                                                    </div>
+                                                </Dialog.Panel>
+                                            </Transition.Child>
                                         </div>
                                     </div>
-                                </div>
-                            </div>
+                                </Dialog>
+                            </Transition>
                         </DynamicPhone>
 
                         <div className="mt-6 lg:mt-0 lg:px-2 lg:w-4/5">
