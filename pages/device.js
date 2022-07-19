c0eb0aa17e797e57b9b777bc286c07106ac22e76
Joowon Moon <jsjs401@daum.net>
2022-07-19 17:23:50 +0900

    Update device.js

--- pages/device.js
+++ pages/device.js
@@ -114,7 +114,7 @@ export default function SsrDevice(ssrUser) {
                                             {!device.using &&
                                                 <button className="flex items-center justify-center w-full px-2 py-2 mt-4 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-gray-800 rounded-md hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
                                                     onClick={ async (event) => {
-                                                        const success = await deviceContHandler("POST", device.serial).sucess;
+                                                        const success = deviceContHandler("POST", device.serial).sucess;
                                                         console.log(success);
                                                         success ?
                                                             Router.push('/control')
@@ -147,7 +147,7 @@ export default function SsrDevice(ssrUser) {
 }
 
 async function deviceContHandler(method, deviceSerial) {
-    await fetchJson(
+    return await fetchJson(
         '/api/deviceCont', {
         method: "POST",
         headers: {
