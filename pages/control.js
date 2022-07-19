4167c37bc83e8796a258219ec7f41e84783f9f50
Joowon Moon <jsjs401@daum.net>
2022-07-19 17:47:39 +0900

    Update control.js

--- pages/control.js
+++ pages/control.js
@@ -18,7 +18,7 @@ export default function Control() {
     return (
         <Layout currentPage={currentPage}>
             <div className='absolute left-1/2 '>
-                <iframe src="http://61.74.187.4:7100/#!/control/R3CT104SAYT" width="1000" height="1000" scrolling="no" frameborder="0"></iframe>
+                <iframe src="http://61.74.187.4:7100/#!/control/R3CT104SAYT" width="1200" height="1200" scrolling="no" frameborder="0"></iframe>
             </div>
         </Layout>
   )
