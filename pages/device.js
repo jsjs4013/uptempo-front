ccd1aef5f1862a2d9b36fc373fd2617a20f2492d
Joowon Moon <jsjs401@daum.net>
2022-07-19 15:53:39 +0900

    Update device.js

--- pages/device.js
+++ pages/device.js
@@ -115,7 +115,7 @@ export default function SsrDevice(ssrUser) {
                                                 <Link href="/[id]" as={`/${device.marketName}`} >
                                                     <button className="flex items-center justify-center w-full px-2 py-2 mt-4 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-gray-800 rounded-md hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
                                                         onClick={ async () => {await fetchJson(
-                                                                '/api/stopUsing', {
+                                                                '/api/deviceCont', {
                                                                 method: "POST",
                                                                 headers: {
                                                                     'Content-Type': 'application/json',
